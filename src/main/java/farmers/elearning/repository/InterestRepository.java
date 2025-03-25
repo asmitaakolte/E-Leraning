764529ebf3583776a16790a5b876c89db4e9e21c
Asmita Arun Kolte <144066811+asmitaakolte@users.noreply.github.com>
2025-03-25 04:40:00 -0500

    Add files via upload

---
+++ src/main/java/farmers/elearning/repository/InterestRepository.java
@@ -0,0 +1,7 @@
+package com.farmers.elearning.repository;
+
+import com.farmers.elearning.model.Interest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InterestRepository extends JpaRepository<Interest, Long> {
+}
